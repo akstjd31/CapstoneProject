12f550b4a341cec4ddfa619aa69e1616cf5197d6
AnSuHan <glorygem195@gmail.com>
2024-03-20 18:10:45 +0900

    애셋 추가 전 임시 커밋

--- Assets/Scripts/Firebase/UserData.cs
+++ Assets/Scripts/Firebase/UserData.cs
@@ -1,8 +1,11 @@
 using Firebase;
 using Firebase.Auth;
 using Firebase.Firestore;
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Web;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
@@ -12,6 +15,7 @@ public class UserData : MonoBehaviour
     private FirebaseAuth auth;
     private string authEmail = "test0311@gmail.com";
     private string authPassword = "asdf";
+    //안드로이드 앱 지문 //https://www.youtube.com/watch?v=AiXIAe6on5M&t=563s
 
     public void SignInWithEmail_Password()
     {
@@ -134,13 +138,159 @@ public class UserData : MonoBehaviour
 
     public void GetGoogleTokens()
     {
-        StartCoroutine(OpenOAuthURL());
+        //StartCoroutine(OpenOAuthURL());
+        StartCoroutine(OpenOAuthURL3());
+        //StartCoroutine(SignInWithGoogle2());
+        //StartCoroutine(OpenOAuthURL2());
+        //StartCoroutine(SignInWithGoogleCoroutine2());
+        //StartCoroutine(SignInWithGoogleCoroutine());  //memory leak & 403 forbidden
+        //StartCoroutine(SigninAnonymously());    //익명 로그인은 정상적으로 동작
         //StartCoroutine(OpenLoginWebview());   //rawImage를 활용한 웹뷰는 컨트롤 할 수 없음
+    }
+
+    IEnumerator OpenOAuthURL3()
+    {
+        auth = FirebaseAuth.DefaultInstance;
+
+        authUrl_second = $"{authorizationEndpoint_second}?response_type=code&client_id={clientId_second}&redirect_uri={new_redirectUri}&scope={scope_second}";
+
+        // Google 로그인 페이지로 이동
+        Application.OpenURL(authUrl_second);
+
+        while(true)
+        {
+            Debug.Log("abs url" + Application.absoluteURL);
+            Debug.Log("data path" + Application.dataPath);
+
+            // 웹 페이지에 대한 GET 요청 생성
+            UnityWebRequest request = UnityWebRequest.Get(authUrl_second);
+
+            // 요청 보내기
+            yield return request.SendWebRequest();
+
+            // 요청이 성공적으로 완료되었는지 확인
+            if (request.result != UnityWebRequest.Result.Success)
+            {
+                Debug.LogError("Failed to open web page: " + request.error);
+                yield break;
+            }
+
+            // 웹 페이지의 내용 출력
+            //Debug.Log("Web page content: " + request.downloadHandler.text);
+        }
+
+        
+    }
+
+    //try02
+    IEnumerator SignInWithGoogle2()
+    {
+        auth = FirebaseAuth.DefaultInstance;
+
+        // 사용자가 구글 로그인 페이지로 이동하도록 URL을 생성합니다.
+        string authUrl = $"https://accounts.google.com/o/oauth2/v2/auth?client_id={clientId}&redirect_uri={redirectUri}&response_type=code&scope={scope_second}";
+
+        // 보안상의 이유로 웹 뷰를 사용하여 구글 로그인을 진행합니다.
+        // 구글 로그인 페이지로 이동
+        Application.OpenURL(authUrl);
+
+
+
+        yield return new WaitForSeconds(1);
+    }
+
+    // 리디렉션 URI에서 인증 코드를 받고 Firebase에 로그인하는 함수
+    public void HandleGoogleSignInCallback(string authorizationCode)
+    {
+        Debug.Log("authorizationCode : " + authorizationCode);
+
+        StartCoroutine(ExchangeAuthorizationCodeForAccessToken(authorizationCode));
+    }
+
+    IEnumerator ExchangeAuthorizationCodeForAccessToken(string authorizationCode)
+    {
+        // 인증 코드를 사용하여 액세스 토큰을 요청합니다.
+        string tokenEndpoint = "https://oauth2.googleapis.com/token";
+        WWWForm form = new WWWForm();
+        form.AddField("code", authorizationCode);
+        form.AddField("client_id", clientId);
+        form.AddField("client_secret", clientSecret);
+        form.AddField("redirect_uri", redirectUri);
+        form.AddField("grant_type", "authorization_code");
+
+        using (WWW www = new WWW(tokenEndpoint, form))
+        {
+            yield return www;
+
+            if (www.error != null)
+            {
+                Debug.LogError("Error exchanging authorization code for access token: " + www.error);
+            }
+            else
+            {
+                // 응답 받은 JSON을 파싱하여 액세스 토큰 추출
+                Dictionary<string, object> responseData = Google.MiniJSON.Json.Deserialize(www.text) as Dictionary<string, object>;
+                string accessToken = responseData["access_token"] as string;
+
+                // Firebase에 액세스 토큰을 사용하여 로그인합니다.
+                Credential credential = GoogleAuthProvider.GetCredential(accessToken, null);
+                auth.SignInWithCredentialAsync(credential).ContinueWith(task =>
+                {
+                    if (task.IsCanceled || task.IsFaulted)
+                    {
+                        Debug.LogError("Firebase authentication failed: " + task.Exception);
+                    }
+                    else
+                    {
+                        FirebaseUser user = task.Result;
+                        Debug.Log("Firebase authentication successful! User ID: " + user.UserId);
+                    }
+                });
+            }
+        }
+    }
+
+    //try01
+    // Google 로그인을 위한 Coroutine
+    IEnumerator OpenOAuthURL2()
+    {
+        auth = FirebaseAuth.DefaultInstance;
+
+        authUrl_second = $"{authorizationEndpoint_second}?response_type=code&client_id={clientId_second}&redirect_uri={new_redirectUri}&scope={scope_second}";
+
+        // Google 로그인 페이지로 이동
+        Application.OpenURL(authUrl_second);
+
+        while(true)
+        {
+            // URL에서 쿼리 스트링을 추출합니다.
+            NameValueCollection queryString = HttpUtility.ParseQueryString(new Uri(new_redirectUri).Query);
+
+            // "code" 매개변수를 사용하여 인증 코드를 가져옵니다.
+            string authorizationCode = queryString["code"];
+
+            if (!string.IsNullOrEmpty(authorizationCode))
+            {
+                // 여기서 인증 코드를 사용하여 Firebase에 로그인하는 등의 작업을 수행합니다.
+                Debug.Log("Authorization Code: " + authorizationCode);
+            }
+            else
+            {
+                Debug.LogError("Authorization Code Not Found.");
+            }
+
+            yield return new WaitForSeconds(1);
+        }
+
+        yield return new WaitForSeconds(1);
     }
 
     IEnumerator OpenOAuthURL()
     {
         auth = FirebaseAuth.DefaultInstance;
+
+        var credential = GoogleAuthProvider.GetCredential(clientId, clientSecret);
+        //var signInTask = auth.SignInWithCredentialAsync(credential);
 
         //redirect URI가 없으면
         //액세스 차단됨: 승인 오류
@@ -159,11 +309,16 @@ public class UserData : MonoBehaviour
 
         while (!Application.absoluteURL.StartsWith(new_redirectUri))
         {
+            credential = GoogleAuthProvider.GetCredential(clientId, clientSecret);
+
             Debug.Log("Application.absoluteURL : " + Application.absoluteURL);
+            Debug.Log("credential : " + credential);
+            Debug.Log("credential : " + credential.Provider);   //google.com
             //Debug.Log("Application.temporaryCachePath : " + Application.temporaryCachePath);
             //Debug.Log("Application.identifier : " + Application.identifier);            //hallym.capstone.photon
             //Debug.Log("Application.cloudProjectId : " + Application.cloudProjectId);    //a7a5451d-c406-4b70-80a9-007d670f7277
-            
+            Debug.Log("get data : " + AppContext.GetData(clientId));
+            Debug.Log("domain : " + AppDomain.CurrentDomain);   //Unity Child Domain
 
             yield return new WaitForSeconds(1);
         }
@@ -193,6 +348,145 @@ public class UserData : MonoBehaviour
         return authCode;
     }
 
+    /*
+    private IEnumerator SigninCustomToken()
+    {
+        auth = FirebaseAuth.DefaultInstance;
+        string customToken = "YOUR_CUSTOM_TOKEN_HERE";
+
+        // 사용자 정의 토큰으로 로그인 시도
+        var authTask = auth.SignInWithCustomTokenAsync(customToken);
+        yield return new WaitUntil(() => authTask.IsCompleted);
+
+        if (authTask.Exception != null)
+        {
+            // 인증 실패한 경우
+            Debug.LogError("Failed to sign in with custom token: " + authTask.Exception.Message);
+        }
+        else
+        {
+            // 인증이 성공한 경우
+            var authResult = authTask.Result;
+            Debug.Log("Signed in with custom token with UID: " + authResult.User.UserId);
+        }
+    }
+    */
+
+    private IEnumerator SignInWithGoogleCoroutine()
+    {
+        string webApiKey = "AIzaSyA0iuKe5o2kge6nz2zHtysWeT1PCUEhWhQ";
+        string googleLoginUrl;
+        string accessToken;
+
+        // 웹 요청을 보낼 URL
+        string url = $"https://www.googleapis.com/identitytoolkit/v3/relyingparty/getProjectConfig?key={webApiKey}";
+
+        // 웹 요청 보내기
+        UnityWebRequest www = UnityWebRequest.Get(url);
+        yield return www.SendWebRequest();
+
+        // 요청이 성공했는지 확인
+        if (www.result != UnityWebRequest.Result.Success)
+        {
+            Debug.LogError("Failed to get project config: " + www.error);
+            yield break;
+        }
+        Debug.Log("request Result is Success : " + www);
+
+        // 구글 프로젝트 설정에서 Web API 키 가져오기
+        webApiKey = JsonUtility.FromJson<ProjectConfigResponse>(www.downloadHandler.text).webApiKey;
+        Debug.Log("webApiKey : " + webApiKey);
+
+        // Google 로그인 URL 생성
+        googleLoginUrl = "https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyAssertion?key=" + webApiKey;
+
+        // 사용자가 로그인한 구글 계정의 액세스 토큰 가져오기
+        accessToken = "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"; // 여기에 사용자의 구글 액세스 토큰을 설정해야 합니다.
+
+        string requestUri = "https://hallym.capstone.photon.firebaseapp.com/";
+
+        // Google 인증 토큰 요청 데이터 생성
+        GoogleAuthRequestData requestData = new GoogleAuthRequestData
+        {
+            postBody = "{\"postBody\":\"id_token=" + accessToken + "&providerId=google.com\",\"requestUri\":\"" + requestUri + "\",\"returnIdpCredential\":true,\"returnSecureToken\":true}"
+        };
+
+        // Google 인증 토큰 요청 보내기
+        www = new UnityWebRequest(googleLoginUrl, "POST");
+        byte[] bodyRaw = System.Text.Encoding.UTF8.GetBytes(JsonUtility.ToJson(requestData));
+        www.uploadHandler = new UploadHandlerRaw(bodyRaw);
+        www.downloadHandler = new DownloadHandlerBuffer();
+        www.SetRequestHeader("Content-Type", "application/json");
+        yield return www.SendWebRequest();
+
+        // 요청이 성공했는지 확인
+        if (www.result != UnityWebRequest.Result.Success)
+        {
+            Debug.LogError("Failed to get Google authentication token: " + www.error);
+            yield break;
+        }
+
+        // Google 인증 결과 데이터 가져오기
+        GoogleAuthResponseData responseData = JsonUtility.FromJson<GoogleAuthResponseData>(www.downloadHandler.text);
+
+        // FirebaseAuth에 Google 인증 토큰 전달하여 인증
+        var authTask = auth.SignInWithCredentialAsync(GoogleAuthProvider.GetCredential(responseData.idToken, null));
+        yield return new WaitUntil(() => authTask.IsCompleted);
+
+        if (authTask.Exception != null)
+        {
+            Debug.LogError("Firebase sign-in failed: " + authTask.Exception.Message);
+            yield break;
+        }
+
+        // 인증된 사용자 정보 가져오기
+        FirebaseUser user = authTask.Result;
+
+        // 인증된 사용자의 정보 출력
+        //statusText.text = "User signed in successfully: " + user.DisplayName + " (UID: " + user.UserId + ")";
+    }
+
+    // 구글 프로젝트 설정 응답 클래스
+    [System.Serializable]
+    private class ProjectConfigResponse
+    {
+        public string webApiKey;
+    }
+
+    // Google 인증 요청 데이터 클래스
+    [System.Serializable]
+    private class GoogleAuthRequestData
+    {
+        public string postBody;
+    }
+
+    // Google 인증 응답 데이터 클래스
+    [System.Serializable]
+    private class GoogleAuthResponseData
+    {
+        public string idToken;
+    }
+
+    //익명 로그인 정상 동작
+    private IEnumerator SigninAnonymously()
+    {
+        auth = FirebaseAuth.DefaultInstance;
+
+        var authTask = auth.SignInAnonymouslyAsync();
+        yield return new WaitUntil(() => authTask.IsCompleted);
+
+        if (authTask.Exception != null)
+        {
+            // 인증 실패한 경우
+            Debug.LogError("Failed to sign in anonymously: " + authTask.Exception.Message);
+        }
+        else
+        {
+            // 인증이 성공한 경우
+            var authResult = authTask.Result;
+            Debug.Log("Signed in anonymously with UID: " + authResult.User.UserId);
+        }
+    }
 
     //https://chat.openai.com/share/6c741b14-c6ab-41de-aecb-01658a21903b
     private IEnumerator GetTokensCoroutine()
