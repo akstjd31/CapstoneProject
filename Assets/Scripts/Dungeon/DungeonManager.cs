0a1a9e4f96eb9f602db19a3321d60f488b4aba6e
leo929 <leo929@naver.com>
2024-03-25 04:09:26 +0900

    DungeonMapGenerate
    in progress

--- Assets/Scripts/Dungeon/DungeonManager.cs
+++ Assets/Scripts/Dungeon/DungeonManager.cs
@@ -4,16 +4,47 @@ using UnityEngine;
 
 public class DungeonManager : MonoBehaviour
 {
-    public static int roomNum = 15;
+    public static int roomNum = 10;
+    //public static GameObject[] endRooms = new GameObject[roomNum];
+    public static List<GameObject> endRooms = new List<GameObject>();
+    public static int endRoomIndexChecker = 0;
+    public static float mapCreateTimer = 0.0f;
+    public bool isMapCreate = false;
+    GameObject farthestRoom;
+    public GameObject farthestRoomMarker;
+    float farthestRoomDistance = 0;
     // Start is called before the first frame update
     void Start()
     {
-
+        //GetComponent<RoomController>().Invoke("CreateRoom", 0.3f);
+        farthestRoomMarker = Instantiate(farthestRoomMarker, this.transform);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(mapCreateTimer);
+        Debug.Log(isMapCreate);
+        if(!isMapCreate)
+        {
+            mapCreateTimer += Time.deltaTime;
+        }
+        if(mapCreateTimer > 1.0f)
+        {
+            isMapCreate = true;
+        }
+        if(isMapCreate)
+        {
+            for(int i = 0; i < endRoomIndexChecker; i++)
+            {
+                float endRoomDistance = Mathf.Sqrt(Mathf.Pow(endRooms[i].transform.position.x,2) + Mathf.Pow(endRooms[i].transform.position.y,2));
+                if(farthestRoomDistance < endRoomDistance)
+                {
+                    farthestRoomDistance = endRoomDistance;
+                    farthestRoom = endRooms[i];
+                }
+            }
+            farthestRoomMarker.transform.position = farthestRoom.transform.position;
+        }
     }
 }
--- Assets/Scripts/Dungeon/RoomController.cs
+++ Assets/Scripts/Dungeon/RoomController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 
-
 public class RoomController : MonoBehaviour
 {
 	public GameObject[] upRooms;
@@ -15,76 +14,174 @@ public class RoomController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        RaycastHit2D hit;
-        mapSpawnPoints = this.gameObject.transform.GetChild(0).GetComponentsInChildren<Transform>();
-        if(DungeonManager.roomNum > 0)
-        {
-            for(int i = 2; i < mapSpawnPoints.Length; i++)
-            {
-                if(mapSpawnPoints[i].name.Contains("Up"))
-                {
-                    hit = Physics2D.Raycast(new Vector2(this.transform.position.x, this.transform.position.y + 0.5f), Vector2.up, LayerMask.GetMask("Room Checker"));
-                    Debug.DrawRay(new Vector2(this.transform.position.x, this.transform.position.y + 0.5f), Vector2.up, Color.red);
-                    if(hit.collider == null)
-                    {
-                        Instantiate(upRooms[Random.Range(0, upRooms.Length-1)], mapSpawnPoints[i]);
-                        DungeonManager.roomNum--;
-                    }
-                    else
-                    {
-                        Debug.Log("1");
-                        Debug.Log(hit.transform.name);
-                    }
-                }
-                else if(mapSpawnPoints[i].name.Contains("Right"))
-                {
-                    hit = Physics2D.Raycast(new Vector2(this.transform.position.x + 0.5f, this.transform.position.y), Vector2.right, LayerMask.GetMask("Room Checker"));
-                    if(hit.collider == null)
-                    {
-                        Instantiate(rightRooms[Random.Range(0, rightRooms.Length-1)], mapSpawnPoints[i]);
-                        DungeonManager.roomNum--;
-                    }
-                    else
-                    {
-                        Debug.Log("2");
-                        Debug.Log(hit.transform.name);
-                    }
-                }
-                else if(mapSpawnPoints[i].name.Contains("Down"))
-                {
-                    hit = Physics2D.Raycast(new Vector2(this.transform.position.x, this.transform.position.y - 0.5f), Vector2.down, LayerMask.GetMask("Room Checker"));
-                    if(hit.collider == null)
-                    {
-                        Instantiate(downRooms[Random.Range(0, downRooms.Length-1)], mapSpawnPoints[i]);
-                        DungeonManager.roomNum--;
-                    }
-                    else
-                    {
-                        Debug.Log("3");
-                        Debug.Log(hit.transform.name);
-                    }
-                }
-                else if(mapSpawnPoints[i].name.Contains("Left"))
-                {
-                    hit = Physics2D.Raycast(new Vector2(this.transform.position.x - 0.5f, this.transform.position.y), Vector2.left, LayerMask.GetMask("Room Checker"));
-                    if(hit.collider == null)
-                    {
-                        Instantiate(leftRooms[Random.Range(0, leftRooms.Length-1)], mapSpawnPoints[i]);
-                        DungeonManager.roomNum--;
-                    }
-                    else
-                    {
-                        Debug.Log("4");
-                        Debug.Log(hit.transform.name);
-                    }
-                }
-            }
-        }
+        Invoke("CreateRoom", 0.3f);
+        Debug.Log(DungeonManager.roomNum);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void CreateRoom()
+    {
+        RaycastHit2D hit;
+        mapSpawnPoints = this.gameObject.transform.GetChild(0).GetComponentsInChildren<Transform>();
+        if(DungeonManager.roomNum > 0)
+        {
+            for(int i = 2; i < mapSpawnPoints.Length; i++)
+            {
+                GameObject roomtemp;
+                if(mapSpawnPoints[i].name.Contains("Up"))
+                {
+                    hit = Physics2D.Raycast(new Vector2(this.transform.position.x, this.transform.position.y + 0.5f), Vector2.up, LayerMask.GetMask("Room Checker"));
+                    Debug.DrawRay(new Vector2(this.transform.position.x, this.transform.position.y + 0.5f), Vector2.up, Color.red);
+                    if(hit.collider == null)
+                    {
+                        roomtemp = Instantiate(upRooms[Random.Range(1, upRooms.Length-1)]);
+                        roomtemp.transform.position = new Vector2(mapSpawnPoints[i].transform.position.x, mapSpawnPoints[i].transform.position.y);
+                        roomtemp.transform.SetParent(this.transform.parent);
+                        DungeonManager.roomNum--;
+                        DungeonManager.mapCreateTimer = 0.0f;
+                    }
+                    // else
+                    // {
+                    //     Debug.Log("1");
+                    // }
+                }
+                else if(mapSpawnPoints[i].name.Contains("Right"))
+                {
+                    hit = Physics2D.Raycast(new Vector2(this.transform.position.x + 0.5f, this.transform.position.y), Vector2.right, LayerMask.GetMask("Room Checker"));
+                    Debug.DrawRay(new Vector2(this.transform.position.x + 0.5f, this.transform.position.y), Vector2.right, Color.red);
+                    if(hit.collider == null)
+                    {
+                        roomtemp = Instantiate(rightRooms[Random.Range(1, rightRooms.Length-1)]);
+                        roomtemp.transform.position = new Vector2(mapSpawnPoints[i].transform.position.x, mapSpawnPoints[i].transform.position.y);
+                        roomtemp.transform.SetParent(this.transform.parent);
+                        DungeonManager.roomNum--;
+                        DungeonManager.mapCreateTimer = 0.0f;
+                    }
+                    // else
+                    // {
+                    //     Debug.Log("2");
+                    // }
+                }
+                else if(mapSpawnPoints[i].name.Contains("Down"))
+                {
+                    hit = Physics2D.Raycast(new Vector2(this.transform.position.x, this.transform.position.y - 0.5f), Vector2.down, LayerMask.GetMask("Room Checker"));
+                    Debug.DrawRay(new Vector2(this.transform.position.x, this.transform.position.y - 0.5f), Vector2.down, Color.red);
+                    if(hit.collider == null)
+                    {
+                        roomtemp = Instantiate(downRooms[Random.Range(1, downRooms.Length-1)]);
+                        roomtemp.transform.position = new Vector2(mapSpawnPoints[i].transform.position.x, mapSpawnPoints[i].transform.position.y);
+                        roomtemp.transform.SetParent(this.transform.parent);
+                        DungeonManager.roomNum--;
+                        DungeonManager.mapCreateTimer = 0.0f;
+                    }
+                    // else
+                    // {
+                    //     Debug.Log("3");
+                    // }
+                }
+                else if(mapSpawnPoints[i].name.Contains("Left"))
+                {
+                    hit = Physics2D.Raycast(new Vector2(this.transform.position.x - 0.5f, this.transform.position.y), Vector2.left, LayerMask.GetMask("Room Checker"));
+                    Debug.DrawRay(new Vector2(this.transform.position.x - 0.5f, this.transform.position.y), Vector2.left, Color.red);
+                    if(hit.collider == null)
+                    {
+                        roomtemp = Instantiate(leftRooms[Random.Range(1, leftRooms.Length-1)]);
+                        roomtemp.transform.position = new Vector2(mapSpawnPoints[i].transform.position.x, mapSpawnPoints[i].transform.position.y);
+                        roomtemp.transform.SetParent(this.transform.parent);
+                        DungeonManager.roomNum--;
+                        DungeonManager.mapCreateTimer = 0.0f;
+                    }
+                    // else
+                    // {
+                    //     Debug.Log("4");
+                    // }
+                }
+            }
+        }
+        else
+        {
+            if(mapSpawnPoints.Length == 1)
+            {
+                DungeonManager.endRooms.Add(this.gameObject);
+                DungeonManager.endRoomIndexChecker++;
+            }
+            for(int i = 2; i < mapSpawnPoints.Length; i++)
+            {
+                GameObject roomtemp;
+                if(mapSpawnPoints[i].name.Contains("Up"))
+                {
+                    hit = Physics2D.Raycast(new Vector2(this.transform.position.x, this.transform.position.y + 0.5f), Vector2.up, LayerMask.GetMask("Room Checker"));
+                    Debug.DrawRay(new Vector2(this.transform.position.x, this.transform.position.y + 0.5f), Vector2.up, Color.red);
+                    if(hit.collider == null)
+                    {
+                        roomtemp = Instantiate(upRooms[0]);
+                        roomtemp.transform.position = new Vector2(mapSpawnPoints[i].transform.position.x, mapSpawnPoints[i].transform.position.y);
+                        roomtemp.transform.SetParent(this.transform.parent);
+                        DungeonManager.roomNum--;
+                        DungeonManager.mapCreateTimer = 0.0f;
+                    }
+                    // else
+                    // {
+                    //     Debug.Log("5");
+                    // }
+                }
+                else if(mapSpawnPoints[i].name.Contains("Right"))
+                {
+                    hit = Physics2D.Raycast(new Vector2(this.transform.position.x + 0.5f, this.transform.position.y), Vector2.right, LayerMask.GetMask("Room Checker"));
+                    Debug.DrawRay(new Vector2(this.transform.position.x + 0.5f, this.transform.position.y), Vector2.right, Color.red);
+                    if(hit.collider == null)
+                    {
+                        roomtemp = Instantiate(rightRooms[0]);
+                        roomtemp.transform.position = new Vector2(mapSpawnPoints[i].transform.position.x, mapSpawnPoints[i].transform.position.y);
+                        roomtemp.transform.SetParent(this.transform.parent);
+                        DungeonManager.roomNum--;
+                        DungeonManager.mapCreateTimer = 0.0f;
+                    }
+                    // else
+                    // {
+                    //     Debug.Log("6");
+                    // }
+                }
+                else if(mapSpawnPoints[i].name.Contains("Down"))
+                {
+                    hit = Physics2D.Raycast(new Vector2(this.transform.position.x, this.transform.position.y - 0.5f), Vector2.down, LayerMask.GetMask("Room Checker"));
+                    Debug.DrawRay(new Vector2(this.transform.position.x, this.transform.position.y - 0.5f), Vector2.down, Color.red);
+                    if(hit.collider == null)
+                    {
+                        roomtemp = Instantiate(downRooms[0]);
+                        roomtemp.transform.position = new Vector2(mapSpawnPoints[i].transform.position.x, mapSpawnPoints[i].transform.position.y);
+                        roomtemp.transform.SetParent(this.transform.parent);
+                        DungeonManager.roomNum--;
+                        DungeonManager.mapCreateTimer = 0.0f;
+                    }
+                    // else
+                    // {
+                    //     Debug.Log("7");
+                    // }
+                }
+                else if(mapSpawnPoints[i].name.Contains("Left"))
+                {
+                    hit = Physics2D.Raycast(new Vector2(this.transform.position.x - 0.5f, this.transform.position.y), Vector2.left, LayerMask.GetMask("Room Checker"));
+                    Debug.DrawRay(new Vector2(this.transform.position.x - 0.5f, this.transform.position.y), Vector2.left, Color.red);
+                    if(hit.collider == null)
+                    {
+                        roomtemp = Instantiate(leftRooms[0]);
+                        roomtemp.transform.position = new Vector2(mapSpawnPoints[i].transform.position.x, mapSpawnPoints[i].transform.position.y);
+                        roomtemp.transform.SetParent(this.transform.parent);
+                        DungeonManager.roomNum--;
+                        DungeonManager.mapCreateTimer = 0.0f;
+                    }
+                    // else
+                    // {
+                    //     Debug.Log("8");
+                    // }
+                }
+            }
+        }
     }
 }
