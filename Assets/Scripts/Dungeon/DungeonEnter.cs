0dccebcec166fd2111a86915b7b1e48f6eafe479
leo929 <leo929@naver.com>
2024-04-10 23:28:32 +0900

    dungeon enter

--- Assets/Scripts/Dungeon/DungeonEnter.cs
+++ Assets/Scripts/Dungeon/DungeonEnter.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 using UnityEngine.SceneManagement;
 
-public class DungeonEnter : MonoBehaviour
+public class DungeonEnter : MonoBehaviourPunCallbacks
 {
+    // 전환할 씬의 이름
+    public string nextSceneName = "DungeonScene";
+    int[] partyUserID = new int[2];
     
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -20,6 +24,29 @@ public class DungeonEnter : MonoBehaviour
 
     void OnCollisionEnter2D()
     {
-        SceneManager.LoadScene("DugeonScene");
+        partyUserID[0] = GetComponent<Party>().GetPartyLeaderID();
+        partyUserID[1] = GetComponent<Party>().GetPartyMemberID();
+        //SceneManager.LoadScene("DungeonScene");
+        RequestSceneSwitch(partyUserID);
+    }
+
+    // 특정 플레이어들에게만 씬 전환을 요청하는 RPC 메서드
+    [PunRPC]
+    void RequestSceneSwitch(int[] playerIDs)
+    {
+        foreach (int playerID in playerIDs)
+        {
+            // 각 플레이어에게 씬 전환 요청을 보냄
+            PhotonView playerView = PhotonView.Find(playerID);
+            playerView.RPC("SwitchScene", RpcTarget.AllBuffered, nextSceneName);
+        }
+    }
+
+    // 씬 전환 메서드
+    [PunRPC]
+    void SwitchScene(string sceneName)
+    {
+        // 포톤 네트워크를 통해 씬 전환을 수행
+        PhotonNetwork.LoadLevel(sceneName);
     }
 }
