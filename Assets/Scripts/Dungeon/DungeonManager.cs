0f82ece4a5be20ef9ee5d5d763f6ac660dbb4715
akstjd31 <akstjd31@naver.com>
2024-04-15 18:26:05 +0900

    .

--- Assets/Scripts/Dungeon/DungeonManager.cs
+++ Assets/Scripts/Dungeon/DungeonManager.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class DungeonManager : MonoBehaviour
@@ -96,7 +95,7 @@ public class DungeonManager : MonoBehaviour
             bossRoomMarker_.transform.position = bossRoom.transform.position;
             specialRoomSelect = true;
 
-            //Debug.Log(farherstMX +" "+ farherstX +" "+ farherstMY +" "+ farherstY);
+            Debug.Log(farherstMX +" "+ farherstX +" "+ farherstMY +" "+ farherstY);
         }
     }
 }
