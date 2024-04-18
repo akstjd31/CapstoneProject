5cc7808f7f77cfc3569e836140e2dafea3159876
AnSuHan <glorygem195@gmail.com>
2024-04-18 22:49:10 +0900

    던전 선택 UI

---
+++ Assets/Scripts/Dungeon/SelectDungeon.cs
@@ -0,0 +1,238 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectDungeon : MonoBehaviour
+{
+    private GameObject DungeonCanvas;
+    private RawImage DungeonImage;
+    private Button destroyButton;
+
+    private Toggle[] dungeonPoints;
+    [SerializeField]
+    private Sprite PointOn;
+    [SerializeField]
+    private Sprite PointOff;
+    private Sprite emptySprite;
+
+    private Toggle nowSelectPoint;
+    [SerializeField]
+    private string dungeonName = "dungeon name";
+    private Text enterName;
+
+    private int mapSizeWidth = 1920;
+    private int mapSizeHeight = 1080;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        MakeDungeonMap();
+    }
+
+    private void SetDungeonPoint()
+    {
+        //position of points
+        List<List<int>> points = new()
+        {
+            new List<int> { mapSizeWidth / 4, mapSizeHeight / 2 },
+            new List<int> { mapSizeWidth / 4, mapSizeHeight / 4 },
+            new List<int> { mapSizeWidth / 4 * 3, mapSizeHeight / 4 },
+            new List<int> { mapSizeWidth / 4 * 3, mapSizeHeight / 4 * 3 },
+        };
+
+        dungeonPoints = new Toggle[points.Count];
+
+        int cnt = 0;
+        ToggleGroup toggleGroup = DungeonCanvas.AddComponent<ToggleGroup>();
+
+        foreach (List<int> point in points)
+        {
+            Toggle toggle;
+            Image bg, checkmark;
+            RectTransform toggleRectTransform;
+
+            //structure
+            toggle = new GameObject("Toggle").AddComponent<Toggle>();
+            bg = new GameObject("Background").AddComponent<Image>();
+            checkmark = new GameObject("Checkmark").AddComponent<Image>();
+
+            toggle.transform.SetParent(DungeonCanvas.transform, false);
+            bg.transform.SetParent(toggle.transform, false);
+            checkmark.transform.SetParent(bg.transform, false);
+
+            //components
+            toggleRectTransform = toggle.GetComponent<RectTransform>();
+            toggleRectTransform.sizeDelta = new Vector2(25, 25);
+            toggle.graphic = checkmark.GetComponent<Image>();
+
+            if (emptySprite == null)
+            {
+                emptySprite = bg.sprite;
+            }
+
+            SetBackground(bg);
+            SetCheckmark(checkmark);
+
+            toggle.gameObject.transform.position = new Vector2(point[0], point[1]);
+            //initial sprite
+            checkmark.sprite = emptySprite;
+            bg.sprite = PointOff;
+
+            toggle.group = toggleGroup;
+
+            toggle.onValueChanged.AddListener((bool isOn) =>
+            {
+                checkmark.sprite = isOn ? PointOn : emptySprite;
+                bg.sprite = isOn ? null : PointOff;
+                nowSelectPoint = toggle;
+
+                dungeonName = toggle.name;
+                enterName.text = dungeonName;
+            });
+
+            dungeonPoints[cnt++] = toggle;
+        }
+    }
+
+    private void SetCheckmark(Image check)
+    {
+        CanvasRenderer canvasRenderer = check.GetComponent<CanvasRenderer>();
+        canvasRenderer.cullTransparentMesh = true;
+
+        check.sprite = PointOff;
+        check.color = Color.white;
+        check.material = null;
+        check.raycastTarget = true;
+        check.maskable = true;
+        check.type = Image.Type.Sliced;
+        check.fillCenter = true;
+    }
+
+    private void SetBackground(Image bg)
+    {
+        CanvasRenderer canvasRenderer = bg.GetComponent<CanvasRenderer>();
+        canvasRenderer.cullTransparentMesh = true;
+
+        bg.color = Color.white;
+        bg.material = null;
+        bg.raycastTarget = true;
+        bg.maskable = true;
+        bg.type = Image.Type.Sliced;
+        bg.fillCenter = true;
+    }
+
+    private void MakeDungeonMap()
+    {
+        DungeonCanvas = new GameObject("Dungeon_Select");
+        DungeonImage = DungeonCanvas.AddComponent<RawImage>();
+        DungeonCanvas.transform.SetParent(GameObject.Find("Canvas").transform, false);
+        DungeonCanvas.GetComponent<RectTransform>().sizeDelta = new Vector2(mapSizeWidth, mapSizeHeight);
+
+        string imagePath = "dungeon_enter_map";
+        Texture2D texture = Resources.Load<Texture2D>(imagePath);
+        DungeonCanvas.GetComponent<RawImage>().texture = texture;
+
+        //button
+        GameObject buttonObject = new GameObject("DestroyButton");
+
+        destroyButton = buttonObject.AddComponent<Button>();
+
+        Text buttonText = buttonObject.AddComponent<Text>();
+        buttonText.text = "X";
+        buttonText.fontSize = 56;
+        buttonText.color = Color.black;
+        
+        buttonObject.transform.SetParent(GameObject.Find("Canvas").transform, false);
+
+        RectTransform buttonTransform = buttonObject.GetComponent<RectTransform>();
+
+        // set button's position
+        buttonTransform.anchorMin = new Vector2(1f, 1f);
+        buttonTransform.anchorMax = new Vector2(1f, 1f);
+        buttonTransform.pivot = new Vector2(1f, 1f);
+        buttonTransform.anchoredPosition = new Vector2(-10f, -10f);
+
+        
+        destroyButton.onClick.AddListener(DisableDungeonCanvas);
+
+        //information of selected dungeon
+        EnterUI();
+
+        //make clickable point
+        SetDungeonPoint();
+
+        DungeonCanvas.SetActive(false);
+    }
+
+    private void EnterUI()
+    {
+        //information of selected dungeon UI
+        Image EnterDungeonUI = new GameObject("Dungeon_Enter").AddComponent<Image>();
+        EnterDungeonUI.transform.SetParent(DungeonCanvas.transform, false);
+
+        RectTransform EnterTransform = EnterDungeonUI.GetComponent<RectTransform>();
+        EnterTransform.anchorMin = new Vector2(0.5f, 0f);
+        EnterTransform.anchorMax = new Vector2(0.5f, 0f);
+        EnterTransform.pivot = new Vector2(0.5f, 0f);
+        EnterTransform.anchoredPosition = new Vector2(0f, mapSizeHeight / 10);
+        EnterTransform.sizeDelta = new Vector2((float)(mapSizeWidth * 0.25), (float)(mapSizeHeight * 0.15));
+
+        EnterDungeonUI.color = new Color(0f, 0f, 0f, 0.7f);
+
+        //button
+        Button btn_enter = new GameObject("btn_Enter").AddComponent<Button>();
+        btn_enter.onClick.AddListener(() =>
+        {
+            Debug.Log("change scene");
+
+        });
+        btn_enter.transform.SetParent(EnterDungeonUI.transform, false);
+
+        CanvasRenderer btnRenderer = btn_enter.gameObject.AddComponent<CanvasRenderer>();
+        RectTransform btnRectTransform = btn_enter.gameObject.AddComponent<RectTransform>();
+        Image btnImage = btn_enter.gameObject.AddComponent<Image>();
+
+        btnRenderer.cullTransparentMesh = true;
+        btnImage.color = new Color(0f, 0f, 0.8f, 1f);
+
+        btnRectTransform.anchorMin = new Vector2(1f, 0.5f);
+        btnRectTransform.anchorMax = new Vector2(1f, 0.5f);
+        btnRectTransform.pivot = new Vector2(1f, 0.5f);
+        btnRectTransform.anchoredPosition = new Vector2(-(float)(EnterTransform.sizeDelta[0] * 0.05), 0f);
+
+        //text
+        enterName = new GameObject("enter_name").AddComponent<Text>();
+        RectTransform nameTransform = enterName.gameObject.GetComponent<RectTransform>();
+
+        enterName.transform.SetParent(EnterDungeonUI.transform, false);
+
+        enterName.text = dungeonName;
+        enterName.alignment = TextAnchor.MiddleLeft;
+        enterName.font = Resources.GetBuiltinResource<Font>("Arial.ttf");
+        enterName.fontSize = 32;
+        enterName.fontStyle = FontStyle.Bold;
+
+        nameTransform.anchorMin = new Vector2(0f, 0.5f);
+        nameTransform.anchorMax = new Vector2(0f, 0.5f);
+        nameTransform.pivot = new Vector2(0f, 0.5f);
+        nameTransform.anchoredPosition = new Vector2((float)(EnterTransform.sizeDelta[0] * 0.05), 0f);
+        nameTransform.sizeDelta = new Vector2((float)(EnterTransform.sizeDelta[0] * 0.65), (float)(EnterTransform.sizeDelta[1] * 0.4));
+    }
+
+    public void DisableDungeonCanvas()
+    {
+        if (DungeonCanvas != null)
+        {
+            DungeonCanvas.SetActive(false);
+        }
+    }
+
+    public void EnableDungeonCanvas()
+    {
+        if (DungeonCanvas != null)
+        {
+            DungeonCanvas.SetActive(true);
+        }
+    }
+}
