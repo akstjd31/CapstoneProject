8782e9627c95b9ef9cf68fa1584c46c0dd8037f3
AnSuHan <82632315+AnSuHan@users.noreply.github.com>
2024-04-23 18:08:13 +0900

    던전 설명 추가 (#49)
    * 던전 맵 확대, 축소
    
    * 던전 설명

---
+++ Assets/Scripts/Dungeon/HandleMap.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class HandleMap : MonoBehaviour, IPointerDownHandler, IDragHandler, IScrollHandler
+{
+    private RectTransform rectTransform;
+    private Vector2 lastPointerPosition;
+
+    private Vector2 minPosition;
+    private Vector2 maxPosition;
+    private Vector2 canvasSize;
+    private Vector2 imageSize;
+
+    private float minScale = 1.0f;
+    private float maxScale = 2.0f;
+    private float currentScale = 1.0f;
+
+    private void Start()
+    {
+        canvasSize = new Vector2(1920, 1080);
+        rectTransform = GetComponent<RectTransform>();
+        CalculateMinMaxPosition();
+    }
+
+    private void CalculateMinMaxPosition()
+    {
+        imageSize = new Vector2(1920, 1080) * currentScale;
+
+        minPosition = (imageSize - canvasSize) * -0.5f;
+        maxPosition = (imageSize - canvasSize) * 0.5f;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        lastPointerPosition = eventData.position;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        CalculateMinMaxPosition();
+
+        Vector2 pointerDelta = eventData.position - lastPointerPosition;
+        Vector2 newPosition = rectTransform.anchoredPosition + pointerDelta;
+
+        // 이미지가 캔버스보다 작은 경우, 드래그가 이동되지 않도록 합니다.
+        if (minPosition.x == 0 && minPosition.y == 0 && maxPosition.x == 0 && maxPosition.y == 0)
+        {
+            return;
+        }
+
+        // 새로운 위치가 드래그 범위를 벗어나지 않도록 제한
+        newPosition.x = Mathf.Clamp(newPosition.x, minPosition.x, maxPosition.x);
+        newPosition.y = Mathf.Clamp(newPosition.y, minPosition.y, maxPosition.y);
+
+        rectTransform.anchoredPosition = newPosition;
+        lastPointerPosition = eventData.position;
+    }
+
+    public void OnScroll(PointerEventData eventData)
+    {
+        float scrollDelta = eventData.scrollDelta.y;
+        currentScale = Mathf.Clamp(currentScale + scrollDelta * 0.05f, minScale, maxScale);
+        rectTransform.localScale = new Vector3(currentScale, currentScale, 1.0f);
+
+        CalculateMinMaxPosition();
+
+        // 이미지가 화면을 벗어나면 가장 가까운 위치로 이동합니다.
+        Vector2 newPosition = rectTransform.anchoredPosition;
+        newPosition.x = Mathf.Clamp(newPosition.x, minPosition.x, maxPosition.x);
+        newPosition.y = Mathf.Clamp(newPosition.y, minPosition.y, maxPosition.y);
+        rectTransform.anchoredPosition = newPosition;
+    }
+}
