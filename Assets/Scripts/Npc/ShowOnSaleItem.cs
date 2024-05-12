a5607ff6f16bdb73c9bf95066d96a5ffcd25b16e
AnSuHan <glorygem195@gmail.com>
2024-05-12 16:51:18 +0900

    npc 배치

--- Assets/Scripts/Npc/ShowOnSaleItem.cs
+++ Assets/Scripts/Npc/ShowOnSaleItem.cs
@@ -14,39 +14,61 @@ public class ShowOnSaleItem : MonoBehaviour
     private GameObject shop;
     private Transform content;
     private List<GameObject> itemList = new List<GameObject>();
+    private Button btn_reroll;
 
-    /*
-    void Start()
+    public void ShowShopUI(int shopType)
     {
-        shop = Instantiate(shopView, GameObject.Find("Canvas").transform);
-        content = shop.transform.Find("Bag/Items/Viewport/Content");
-        //SetOnsaleList();
-
-        Debug.Log("called start");
-    }
-    */
-
-    public void ShowShopUI()
-    {
-        shop = Instantiate(shopView, GameObject.Find("Canvas").transform);
-        content = shop.transform.Find("Bag/Items/Viewport/Content");
-
-        if(content == null)
+        if (shop == null)
         {
-            Debug.LogError("Can't Find ScrollView's content object");
-            return;
-        }
-        Debug.Log($"content name : {shop.name} {content.name} / {content.transform.parent}");
+            shop = Instantiate(shopView, GameObject.Find("Canvas").transform);
+            content = shop.transform.Find("Bag/Items/Viewport/Content");
+            //btn_reroll = GameObject.Find("ButtonReroll").GetComponent<Button>();
 
-        SetOnsaleList();
+            if (content == null)
+            {
+                Debug.LogError("Can't Find ScrollView's content object");
+                return;
+            }
+
+            GameObject.Find("CloseButton").GetComponent<Button>().onClick.AddListener(() =>
+            {
+                CloseShopUI();
+            });
+            btn_reroll?.onClick.AddListener(() =>
+            {
+                Debug.Log("clicked reroll");
+                Reroll();
+            });
+        }
+
+        shop.SetActive(true);
+        //Debug.Log($"content name : {shop.name} {content.name} / {content.transform.parent}");
+
+        SetOnsaleList(shopType);
     }
 
     public void CloseShopUI()
     {
-        itemList = null;
-        Destroy(shop);
+        itemList = new List<GameObject>();
+        //판매 중이었던 아이템 삭제
+        foreach (Transform child in content)
+        {
+            Destroy(child.gameObject);
+        }
 
-        Debug.Log($"Destroy : {shop == null} {content == null}");
+        shop.SetActive(false);
+    }
+
+    private void Reroll()
+    {
+        itemList = new List<GameObject>();
+        //판매 중이었던 아이템 삭제
+        foreach (Transform child in content)
+        {
+            Destroy(child.gameObject);
+        }
+
+        SetOnsaleList();
     }
 
     private void SetOnsaleList(int shopKind = 1001)
@@ -54,6 +76,7 @@ public class ShowOnSaleItem : MonoBehaviour
         GameObject temp;
         ItemSell showOnSaleItem = FindObjectOfType<ItemSell>();
 
+        //이 부분이 Reroll의 영향을 받음
         Dictionary<int, List<string>> items = showOnSaleItem.GetShopItemList(shopKind);
 
         int numOfItem = items.Count;
